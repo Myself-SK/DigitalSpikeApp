8952d64eb7c05f390f16b4f87cfeec1f1bc625e8
ShivaKumar <mail.shivaKumar03@gmail.com>
2023-09-07 15:47:28 +0530

    Add and Remove Employees and Project in Team

--- Team/routes.js
+++ Team/routes.js
@@ -120,8 +120,117 @@ TeamRouter.get("/getTeam/:id", async (req, res) => {
 TeamRouter.post("/addProject/:id", async (req, res) => {
   try {
     const team = await Team.findById(req.params.id);
-    console.log(team);
-  } catch (err) {}
+    proList = [];
+    for (pro of team.projects) {
+      proList.push(pro);
+    }
+    for (pro of req.body.projects) {
+      const proj = await Project.findOne({
+        domainName: pro,
+      });
+      proList.push(proj._id);
+    }
+    const updatedTeam = await Team.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        projects: proList,
+      }
+    );
+    res.status(200).json({ message: "Projects added Successfully" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+TeamRouter.post("/removeProject/:id", async (req, res) => {
+  try {
+    const team = await Team.findById(req.params.id);
+    proList = [];
+    for (pro of team.projects) {
+      const proj = await Project.findById(pro);
+      for (proName of req.body.projects) {
+        if (proName != proj.domainName) {
+          proList.push(proj._id);
+        }
+      }
+    }
+    console.log(proList);
+    const updatedTeam = await Team.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        projects: proList,
+      }
+    );
+    res.status(200).json({ message: "Projects removed Successfully" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+TeamRouter.post("/addEmployee/:id", async (req, res) => {
+  try {
+    const team = await Team.findById(req.params.id);
+    empList = [];
+    for (emp of team.teamMembers) {
+      empList.push(emp);
+    }
+    for (emp of req.body.teamMembers) {
+      const empl = await Employee.findOne({
+        userID: emp,
+      });
+      empList.push(empl._id);
+    }
+    const updatedTeam = await Team.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        teamMembers: empList,
+      }
+    );
+    res.status(200).json({ message: "Employees added Successfully" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+TeamRouter.post("/removeEmployee/:id", async (req, res) => {
+  try {
+    const team = await Team.findById(req.params.id);
+    empList = [];
+    for (emp of team.teamMembers) {
+      const empl = await Employee.findById(emp);
+      for (empName of req.body.teamMembers) {
+        if (empName != empl.userID) {
+          empList.push(empl._id);
+        }
+      }
+    }
+    console.log(empList);
+    const updatedTeam = await Team.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        teamMembers: empList,
+      }
+    );
+    res.status(200).json({ message: "Employees removed Successfully" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
 });
 
 TeamRouter.delete("/deleteTeam/:id", async (req, res) => {
